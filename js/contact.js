7a4dc1c7ecb293e4c72330e19a561cccc2b8adb3
nouramohamed-eng <nouramohamed20099@gmail.com>
2026-08-10 23:56:43 +0300

    finished contact page style and its functionality

---
+++ js/contact.js
@@ -0,0 +1,71 @@
+//selectors
+const menuBtn = document.querySelector('.toggle-menue');
+const navLinks = document.querySelector('.nav-links');
+const toggleButton = document.querySelector(".toggle-button");
+const JowakLogo = document.querySelector(".logo")
+const NameInp =document.querySelector(".nameInp")
+const EmailInp = document.querySelector(".emailInp")
+const PhoneInp = document.querySelector(".PhoneInp")
+const SubInp = document.querySelector(".SubInp")
+const MessInp=document.querySelector(".MessageInp")
+const SendBtn = document.querySelector(".SendBtn")
+const form = document.querySelector(".form")
+//dark-light theme button
+toggleButton.addEventListener(("click"),()=>{
+document.body.classList.toggle("light-theme")
+})
+
+//jowak logo 
+function jowakclick() {
+    window.location.href = 'home.html';
+}
+JowakLogo.addEventListener('click', jowakclick);
+
+//sidebar menue
+if (menuBtn && navLinks){
+menuBtn.addEventListener("click",(e)=>{
+    e.stopPropagation()
+    navLinks.classList.toggle('open')
+})
+document.addEventListener('click', (e) => {
+            if (navLinks.classList.contains('open') && !navLinks.contains(e.target)) {
+                navLinks.classList.remove('open');
+            }
+        });
+} 
+
+//form validation
+SendBtn.addEventListener("click", function(e){
+    e.preventDefault();
+
+
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(NameInp.value.trim() === "" || EmailInp.value.trim() === "" || PhoneInp.value.trim() === "" || MessInp.value.trim() === ""){
+        alert("Please Fill Required Fields");
+    } 
+    
+    else if(!EmailInp.value.match(emailPattern)) {
+        alert("Please enter a valid email address");
+    }
+    else {
+        const formData = {
+            name: NameInp.value,
+            email: EmailInp.value,
+            phone: PhoneInp.value,
+            subject: SubInp.value,
+            message: MessInp.value
+        };
+
+        localStorage.setItem("userMessage", JSON.stringify(formData));
+        console.log(formData);
+        
+        alert("Your Response Has Been Recorded and Saved!");
+
+        NameInp.value = "";
+        EmailInp.value = "";
+        PhoneInp.value = "";
+        SubInp.value = "";
+        MessInp.value = "";
+    }
+});
